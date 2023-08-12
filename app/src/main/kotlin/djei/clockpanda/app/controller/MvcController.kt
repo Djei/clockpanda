679da95ace57f6b78ba9664d8dd94c4dee186cd8
Jason Lee Ken Sang <jason.lee@elemy.com>
2023-08-12 23:18:37 +0100

    [feature]: implemented simple user preferences save page

--- app/src/main/kotlin/djei/clockpanda/app/controller/MvcController.kt
+++ app/src/main/kotlin/djei/clockpanda/app/controller/MvcController.kt
@@ -1,12 +1,34 @@
 package djei.clockpanda.app.controller
 
+import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.TimeZone
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class MvcController {
     @RequestMapping("/")
-    fun index(): String {
+    fun index(model: Model): String {
+        // available timezones in America, Europe, and Asia
+        val availableTimeZones = TimeZone.availableZoneIds
+            .filter { it.startsWith("America") || it.startsWith("Europe") || it.startsWith("Asia") }
+            .sorted()
+        model.addAttribute("availableTimeZones", availableTimeZones)
+
+        // available times with 15 minute intervals
+        val availableTimes = mutableListOf<String>()
+        for (hour in 0..23) {
+            for (minute in 0..45 step 15) {
+                availableTimes.add("%02d:%02d".format(hour, minute))
+            }
+        }
+        model.addAttribute("availableTimes", availableTimes)
+
+        // available days of the week
+        val availableDays = DayOfWeek.values().map { it.name }
+        model.addAttribute("availableDays", availableDays)
+
         return "index"
     }
 }
